9c277bd2cf5a5877276a0bf71218133f8cd46770
Leila <leila.boukella@gmail.com>
2022-07-22 17:47:00 -0700

    added some questions

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//Make questions option buttons appear on start
+//hide original elements
+//make timer start on button click
+//create questions array
+
+var questions = ["Which of the following is not a JavaScript data type?"] 
+["What does CSS stand for?"]
+["What is an array in coding?"]
+["Which of the following is NOT a common coding language"]
+;
+
+var options = 
+["boolean, string, cycle, number"]
+["Cascading style sheet, Closing site style, Corrisponding style sheet, Capital sigil skink"]
+["collection of stored data, a variety of languages, to dress someone in clothes, place in readiness"]
+["Javasript, Python, Swift, Emolga"]
+
