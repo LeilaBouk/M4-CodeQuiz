d061ca368832d88be6cebfd565319334275a56bd
Leila <leila.boukella@gmail.com>
2022-07-22 22:10:20 -0700

    connected script to html and set up button with event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,38 @@
 //make timer start on button click
 //create questions array
 
-var questions = ["Which of the following is not a JavaScript data type?"] 
-["What does CSS stand for?"]
-["What is an array in coding?"]
-["Which of the following is NOT a common coding language"]
-;
+var quiz = [
+    {
+        question: "Which of the following is not a JavaScript data type?",
+        options: ["boolean", "string", "cycle", "number"],
+        answer: "cycle"
+    },
+    {
+        question: "What does CSS stand for?",
+        options: ["Cascading style sheet", "Closing site style", "Corrisponding style sheet", "Capital sigil skink"],
+        answer: "Cascading style sheet"
+    },
+    {
+        question: "What is an array in coding?",
+        options: ["collection of stored data", "a variety of languages", "to dress someone in clothes", "place in readiness"],
+        answer: "collection of stored data"
+    },
+    {
+        question: "Which of the following is NOT a common coding language",
+        options: ["Javasript", "Python", "Swift", "Emolga"],
+        answer: "Emolga"
+    },
+    {
+        question:"",
+        options: [""],
+        answer: ""
+    },
+]
 
-var options = 
-["boolean, string, cycle, number"]
-["Cascading style sheet, Closing site style, Corrisponding style sheet, Capital sigil skink"]
-["collection of stored data, a variety of languages, to dress someone in clothes, place in readiness"]
-["Javasript, Python, Swift, Emolga"]
+var startGame = document.querySelector("#start");
+
+startGame.addEventListener("click", function() {
+    console.log("game started!!");
+});
+
 
